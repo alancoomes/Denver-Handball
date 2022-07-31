c10a173fef393984becac40b3276e897040cfafe
Alan Coomes <acoomes33@gmail.com>
2022-07-31 16:26:19 -0600

    added to header and logo components

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,17 @@
 import NavLink from "../../elements/NavLink/NavLink";
+import Link from "react-router-dom";
 
 function Header(props) {
-  return <div></div>;
+  return (
+    <div>
+      <NavLink title="Home"></NavLink>
+      <NavLink title="What Is Handball?" link=""></NavLink>
+      <NavLink title="About Us" link=""></NavLink>
+      <NavLink title="Media" link=""></NavLink>
+      <NavLink title="Contact" link=""></NavLink>
+      <NavLink title="Sponsors" link=""></NavLink>
+    </div>
+  );
 }
 
 export default Header;
---
+++ src/elements/Logo/Logo.js
@@ -0,0 +1,11 @@
+function Logo(props) {
+  return (
+    <div>
+      <a href={props.link}>
+        <img src="../../assets/"></img>
+      </a>
+    </div>
+  );
+}
+
+export default Logo;
