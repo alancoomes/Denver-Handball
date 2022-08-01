b1b483ee4988dd3f606c458e7b97b8de8733644b
Alan Coomes <acoomes33@gmail.com>
2022-07-31 21:31:58 -0600

    updated logo component

--- src/elements/Logo/Logo.js
+++ src/elements/Logo/Logo.js
@@ -2,11 +2,11 @@ import { Link } from "react-router-dom";
 
 function Logo(props) {
   return (
-    <div>
+    <>
       <Link href={props.link}>
         <img src="../../assets/IMG_1792.jpeg" alt="logo"></img>
       </Link>
-    </div>
+    </>
   );
 }
 
