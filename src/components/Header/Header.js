1af9bf6ad91a0cf7be993196ec33bd82aa98445a
Alan Coomes <acoomes33@gmail.com>
2022-07-31 21:31:34 -0600

    updated logo component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,13 @@
+import Logo from "../../elements/Logo/Logo";
 import NavLink from "../../elements/NavLink/NavLink";
 
-function Header(props) {
+function Header() {
   return (
     <header>
       <NavLink title="Home" link="/"></NavLink>
       <NavLink title="What Is Handball?" link="/handball"></NavLink>
       <NavLink title="About Us" link="/about"></NavLink>
+      <Logo link="/"></Logo>
       <NavLink title="Media" link="/media"></NavLink>
       <NavLink title="Contact" link="/contact"></NavLink>
       <NavLink title="Sponsors" link="/sponsors"></NavLink>
--- src/elements/Logo/Logo.js
+++ src/elements/Logo/Logo.js
@@ -1,9 +1,11 @@
+import { Link } from "react-router-dom";
+
 function Logo(props) {
   return (
     <div>
-      <a href={props.link}>
-        <img src="../../assets/"></img>
-      </a>
+      <Link href={props.link}>
+        <img src="../../assets/IMG_1792.jpeg" alt="logo"></img>
+      </Link>
     </div>
   );
 }
