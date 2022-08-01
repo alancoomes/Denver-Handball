8431c44c64191312ccfe5f6769853dbc487a85aa
Alan Coomes <acoomes33@gmail.com>
2022-07-31 21:03:17 -0600

    updated App.js structure

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,1 +1,15 @@
-import React from "react";
+import { render } from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+import Header from "../components/Header/Header";
+
+const App = () => {
+  return (
+    <>
+      <Router>
+        <Header></Header>
+      </Router>
+    </>
+  );
+};
+
+render(<App />, document.getElementsById("root"));
